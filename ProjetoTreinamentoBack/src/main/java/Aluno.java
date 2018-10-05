6e6ae9eb9486b5b7c8f9b24396b596035861faa1
Emanuel <Emanuel@Emanuel-PC>
2018-10-05 19:47:59 -0300

    Metodos get e set da classe Aluno

--- ProjetoTreinamentoBack/src/main/java/Aluno.java
+++ ProjetoTreinamentoBack/src/main/java/Aluno.java
@@ -6,5 +6,32 @@ public class Aluno {
 	private String curso;
 	private Integer matricula;
 	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getCurso() {
+		return this.curso;
+	}
+	
+	public Integer getMatricula() {
+		return this.matricula;
+	}
+	
+	public long getId() {
+		return this.id;
+	}
 
 }
